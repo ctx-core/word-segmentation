9e2a480221ad6e83feb35f0d4dee080be48d449e
Brian Takita <brian.takita@gmail.com>
2020-12-16 23:28:20 -0700

    interface instead of type: aspell_queue_obj_type,words_segment_opts_type

--- aspell_queue_obj_type.ts
+++ aspell_queue_obj_type.ts
@@ -1,4 +1,4 @@
-export type aspell_queue_obj_type = {
+export interface aspell_queue_obj_type {
 	word: string
 	resolve(word_a1: null|string[]): void
 	reject(error: Error): void
--- words_segment_opts_type.d.ts
+++ words_segment_opts_type.d.ts
@@ -1,6 +1,6 @@
 import type { Aspell__top } from './Aspell__top';
 import type { Aspell__compound } from './Aspell__compound';
-export declare type words_segment_opts_type = {
+export interface words_segment_opts_type {
     top_aspell: Aspell__top;
     compound_aspell: Aspell__compound;
-};
+}
